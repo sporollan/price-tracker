d619dff8d6634294bcaafd5a61190b6c2eb606b3
sporollan <santiago.porollan@gmail.com>
2024-06-08 21:02:42 -0300

    add allowed origins

--- src/scraper-service/app.py
+++ src/scraper-service/app.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+# Dependency
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post('/tracked')
 def create_tracked():
