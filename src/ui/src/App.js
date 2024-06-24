b7339dc698e620bb77c4f51d814bdb8477d57383
sporollan <santiago.porollan@gmail.com>
2024-06-23 23:35:44 -0300

    increase margin, make tracked bar sticky

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -12,12 +12,15 @@ function App() {
     <div className="main_container">
         <div className='header'><Header/></div>
         <div className='body_container'>
+          <div className='left_banner'>
+          </div>
           <div className='left_container'>
             <Tracked setProducts={setProducts}/>
           </div>
           <div className='right_container'>
             <Products products={products}/>
           </div>
+          <div className='right_banner'></div>
         </div>
     </div>
   );
--- src/ui/src/components/Header/Header.jsx
+++ src/ui/src/components/Header/Header.jsx
@@ -3,8 +3,16 @@ import './Header.styles.css'
 
 const Header = () => {
     return (
-        <div className="header_bar">
-            <span>Price Tracker</span>
+        <div className='header_container'>
+            <div className="header_bar">
+                <span>Price Tracker</span>
+            </div>
+            <div className='header_middle'>
+
+            </div>
+            <div className='header_bottom'>
+                
+            </div>
         </div>
     )
 }
