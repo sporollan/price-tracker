968bcae919af862d009b3dfde3449073c9f992cc
sporollan <santiago.porollan@gmail.com>
2025-02-16 19:29:58 -0300

    add tests

--- src/scraper-service/tests/test_api.py
+++ src/scraper-service/tests/test_api.py
@@ -1,86 +1,85 @@
+import os
+import requests
 from unittest.mock import patch
-from fastapi.testclient import TestClient
-from main import app
 
-client = TestClient(app)
+BASE_URL = os.getenv("BASE_URL", "http://localhost:8001")
 
 def test_root_endpoint_success():
-    response = client.get("/")
+    response = requests.get(f"{BASE_URL}/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-# test get tracked endpoint
+# Test cases for get tracked endpoint
 def test_get_tracked_endpoint_success():
-    response = client.get("/tracked")
+    response = requests.get(f"{BASE_URL}/tracked")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     for item in response.json():
-        assert "id" in item
-        assert "name" in item
-        assert "sites" in item
-        assert "date_added" in item
-        assert "last_scraped" in item
-        assert "is_active" in item
-        assert isinstance(item["date_added"], int)
-        assert isinstance(item["last_scraped"], int)
-        assert isinstance(item["is_active"], bool)
-
+        assert 'id' in item
+        assert 'name' in item
+        assert 'sites' in item
+        assert 'date_added' in item
+        assert 'last_scraped' in item
+        assert 'is_active' in item
+        assert isinstance(item['date_added'], int)
+        assert isinstance(item['last_scraped'], int)
+        assert isinstance(item['is_active'], bool)
 
 def test_get_tracked_endpoint_failure():
-    response = client.get("/tracked/9123")
+    response = requests.get(f"{BASE_URL}/tracked/12340")
     assert response.status_code == 404
 
-# Test cases for create_tracked endpoint
+# Test cases for create tracked endpoint
 def test_create_tracked_endpoint_success():
-    response = client.post("/tracked", json={"name": "cafe", "sites":"COTO"})
+    response = requests.post(f"{BASE_URL}/tracked", json={'name': 'cafe', 'sites': 'COTO'})
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
-    assert "id" in response.json()
-    assert "name" in response.json()
-    assert "sites" in response.json()
-    assert "date_added" in response.json()
-    assert "last_scraped" in response.json()
-    assert "is_active" in response.json()
-    assert isinstance(response.json()["date_added"], int)
-    assert isinstance(response.json()["last_scraped"], int)
-    assert isinstance(response.json()["is_active"], bool)
+    assert 'id' in response.json()
+    assert 'name' in response.json()
+    assert 'sites' in response.json()
+    assert 'date_added' in response.json()
+    assert 'last_scraped' in response.json()
+    assert 'is_active' in response.json()
+    assert isinstance(response.json()['date_added'], int)
+    assert isinstance(response.json()['last_scraped'], int)
+    assert isinstance(response.json()['is_active'], bool)
 
 def test_create_tracked_endpoint_duplicate():
-    response = client.post("/tracked", json={"name": "cafe1", "sites":"COTO"})
+    response = requests.post(f"{BASE_URL}/tracked", json={'name': 'cafe1', 'sites': 'COTO'})
     assert response.status_code == 200
-    response = client.post("/tracked", json={"name": "cafe1", "sites":"COTO"})
+    response = requests.post(f"{BASE_URL}/tracked", json={'name': 'cafe1', 'sites': 'COTO'})
     assert response.status_code == 409
-    assert response.json() == {"detail": "Name already exists"}
+    assert response.json() == {'detail': 'Name already exists'}
 
 def test_create_tracked_endpoint_invalid_name():
-    response = client.post("/tracked", json={"name": "", "sites":"COTO"})
+    response = requests.post(f"{BASE_URL}/tracked", json={'name': '', 'sites': 'COTO'})
     assert response.status_code == 422
-    assert response.json() == {"detail": "Name cannot be null"}
+    assert response.json() == {'detail': 'Name cannot be null'}
 
-# Test toggle
+# Test toggle endpoint
 def test_toggle_endpoint():
-    response = client.put('/toggle/1')
+    response = requests.put(f"{BASE_URL}/toggle/1")
     assert response.status_code == 200
     assert response.json() == {"is_active": False}
-    response = client.put('/toggle/1')
+    response = requests.put(f"{BASE_URL}/toggle/1")
     assert response.status_code == 200
     assert response.json() == {"is_active": True}
 
 def test_toggle_endpoint_not_found():
-    response = client.put('/toggle/999')
+    response = requests.put(f"{BASE_URL}/toggle/999")
     assert response.status_code == 404
     assert response.json() == {"detail": "Tracked item not found"}
 
-# Test run_scraper_all
+# Test run_scraper_all endpoint success
 def test_run_scraper_all_endpoint_success():
     with patch('scraper.Scraper.run') as mock_run:
         mock_run.return_value = None
-        response = client.post("/run_scraper")
+        response = requests.post(f"{BASE_URL}/run_scraper")
     assert response.json() == {'message': 'Scraping all tracked items completed successfully.'}
     assert response.status_code == 200
+
 def test_run_scraper_all_endpoint_no_new_data():
     with patch('scraper.Scraper.run') as mock_run:
         mock_run.return_value = None
-        response = client.post("/run_scraper")
-        assert response.status_code == 200
+        response = requests.post(f"{BASE_URL}/run_scraper")
         assert response.json() == {'message': 'No new data available for scraping today'}
