4667858d80bfea04622811541c84e06129166237
sporollan <santiago.porollan@gmail.com>
2025-03-20 20:56:31 -0300

    Add auth-service

--- util/build-images.sh
+++ util/build-images.sh
@@ -8,3 +8,4 @@ eval $(minikube -p price-tracker docker-env)
 docker build -t product-service:dev ./src/product-service/
 docker build -t scraper-service:dev ./src/scraper-service/
 docker build -t ui:dev ./src/ui/
+docker build -t auth-service:dev ./src/auth-service/
