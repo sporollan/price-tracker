51a973d4f0dfd0894bc60555f144bb3738845741
sporollan <santiago.porollan@gmail.com>
2025-04-16 17:43:18 -0300

    Update build-images

--- util/build-images.sh
+++ util/build-images.sh
@@ -1,9 +1,3 @@
-eval export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://192.168.49.2:2376"
-export DOCKER_CERT_PATH="/home/sporollan/.minikube/certs"
-export MINIKUBE_ACTIVE_DOCKERD="minikube"
-
-# To point your shell to minikube's docker-daemon, run:
 eval $(minikube -p price-tracker docker-env)
 docker build -t product-service:dev ./src/product-service/
 docker build -t scraper-service:dev ./src/scraper-service/
