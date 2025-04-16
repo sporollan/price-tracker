5811c1af2422554b195de7a6123aa103c89d1a01
sporollan <santiago.porollan@gmail.com>
2025-04-16 18:10:46 -0300

    Update

--- util/build-images.sh
+++ util/build-images.sh
@@ -1,3 +1,4 @@
+# Build images on minikube docker environment
 eval $(minikube -p price-tracker docker-env)
 docker build -t product-service:dev ./src/product-service/
 docker build -t scraper-service:dev ./src/scraper-service/
