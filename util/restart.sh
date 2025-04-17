ca5cd5de9e92b8e5cb9022e14ea61817d2f38721
sporollan <santiago.porollan@gmail.com>
2025-04-17 19:58:59 -0300

    Add sleep between restarts

--- util/restart.sh
+++ util/restart.sh
@@ -1,5 +1,8 @@
 
 kubectl rollout restart deploy auth-service
+sleep 30
 kubectl rollout restart deploy product-service
+sleep 30
 kubectl rollout restart deploy scraper-service
+sleep 30
 kubectl rollout restart deploy ui
